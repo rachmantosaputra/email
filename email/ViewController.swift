a0853b2ef8d9645fd59482ac67bd6e8034c75055
Rachmanto Saputra <rachmanto.saputra@8investment.com>
2020-11-14 07:54:26 +0700

    Initial Commit

---
+++ email/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  email
+//
+//  Created by Wida Putri on 14/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
